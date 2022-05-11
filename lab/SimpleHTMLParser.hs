3fa8492404ac6fd69e6d4953374e757fbcf3a9dd
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-11 16:14:48 -0500

    added function types

--- lab/SimpleHTMLParser.hs
+++ lab/SimpleHTMLParser.hs
@@ -1,13 +1,18 @@
 
+styleAttr :: [Char] -> [Char]
 styleAttr s = " style='" ++ s ++ "'"
 
+openTag :: [Char] -> [Char] -> [Char]
 openTag t s = "<" ++ t ++ styleAttr s ++ ">"
 
+closeTag :: [Char] -> [Char]
 closeTag t = "</" ++ t ++ ">"
 
+element :: [Char] -> [Char] -> [Char] -> [Char]
 element t s c = openTag t s ++ c ++ closeTag t
 
 
+extractTag :: [Char] -> [Char]
 extractTag = pTag ""
     where
         pTag name [] = reverse name
@@ -16,6 +21,7 @@ extractTag = pTag ""
             then pTag name xs
             else pTag (x : name) xs
 
+parseTag :: [Char] -> [[Char]]
 parseTag = toks [] []
     where
         toks cur res [] = reverse (addItem cur res)
@@ -25,6 +31,7 @@ parseTag = toks [] []
             else toks (x:cur) res xs
         addItem c r = reverse c:r
 
+parseElement :: [Char] -> [[[Char]]]
 parseElement = pIter [] []
     where
         pIter cur res [] = reverse res
