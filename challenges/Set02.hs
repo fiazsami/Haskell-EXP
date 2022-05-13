69caf1990b78a06641754a1bf92372056dfd4034
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-12 21:43:16 -0500

    added 14 practice exercises

---
+++ challenges/Set02.hs
@@ -0,0 +1,21 @@
+import Data.Char (isNumber)
+
+
+-- >>> getMinMax [1, -2, -5, -4, 2, 3, 4, -2]
+-- (-5,4)
+
+
+
+-- >>> amplify 3 ['a', '-', 'b', '/', 'c', '*']
+-- "aaa---bbb///ccc***"
+
+
+
+
+-- >>> getDigits "3j4j1k2k33,3j3h3b3h2h2m2k2;;2"
+-- "341233333322222"
+
+
+
+-- >>> splice 3 6 "abcdefghijkl"
+-- "abcghijkl"
---
+++ challenges/Set03.hs
@@ -0,0 +1,19 @@
+-- >>> toChange 94
+-- [(3,25),(1,10),(1,5),(4,1)]
+
+
+
+
+-- >>> readChange (toChange 94)
+-- [(3,"quarters"),(1,"dime"),(1,"nickel"),(4,"pennies")]
+
+
+
+
+-- >>> rotate [1, 2, 3, 4, 5, 6]
+-- [6,2,4,3,5,1]
+
+
+
+-- >>> acronym "central intelligence agency"
+-- "cia"
