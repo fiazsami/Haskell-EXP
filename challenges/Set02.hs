804c8b123a5db1bac656b8babbcd676f20ed0511
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-12 22:25:21 -0500

    added additional challenges

--- challenges/Set02.hs
+++ challenges/Set02.hs
@@ -6,14 +6,25 @@ import Data.Char (isNumber)
 
 
 
--- >>> amplify 3 ['a', '-', 'b', '/', 'c', '*']
--- "aaa---bbb///ccc***"
-
+-- >>> sumDigits "123456"
+-- 21
 
 
 
 -- >>> getDigits "3j4j1k2k33,3j3h3b3h2h2m2k2;;2"
 -- "341233333322222"
+
+
+
+-- >>> shiftR 2 [1, 2, 3, 4, 5]
+-- [5,4,1,2,3]
+
+
+
+
+-- >>> amplify 3 ['a', '-', 'b', '/', 'c', '*']
+-- "aaa---bbb///ccc***"
+
 
 
 
