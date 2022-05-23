3ea794e04c4e1c4af293d09029ef3fb2bbafd102
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-23 17:20:30 -0500

    added fold session file

---
+++ sessions/Folds01.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Redundant lambda" #-}
+{-# HLINT ignore "Avoid lambda" #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use foldr" #-}
+
+
