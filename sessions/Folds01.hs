67141647eb50b81066cc1d5168f1add77ba2c927
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-24 09:48:01 -0500

    deriviation of foldr

--- sessions/Folds01.hs
+++ sessions/Folds01.hs
@@ -6,21 +6,27 @@
 {-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Use foldr" #-}
 
-
 myFilter :: Foldable t => (a -> Bool) -> t a -> [a]
-myFilter fn dataSet = foldr (\x acc -> if fn x then acc else x : acc) [] dataSet 
+myFilter fn dataSet = foldr (\x acc -> if fn x then acc else x : acc) [] dataSet
+
+myFilter' :: (a -> Bool) -> [a] -> [a]
+myFilter' fn dataList = myFoldr (\x acc -> if fn x then acc else x : acc) [] dataList
 
 myFilter0 :: [Integer]
-myFilter0 = myFilter even [1..50]
+myFilter0 = myFilter even [1..12]
 
 myFilter1 :: [Integer]
-myFilter1 = foldr (\x acc -> if even x then acc else x : acc) [] [1..50]
+myFilter1 = foldr (\x acc -> if even x then acc else x : acc) [] [1..12]
 
 fn :: Integer -> [Integer] -> [Integer]
 fn = (\x acc -> if even x then acc else x : acc)
 
 myFilter2 :: [Integer]
-myFilter2 = foldr fn [] [1..50]
+myFilter2 = foldr fn [] [1..12]
 
+myFilter3 :: Foldable t => p1 -> p2 -> [a1] -> (a2 -> b -> b) -> b -> t a2 -> b
+myFilter3 f z (x:xs) = foldr
+
+myFoldr :: (t1 -> t2 -> t2) -> t2 -> [t1] -> t2
 myFoldr f z [] = z
-myFoldr f z (x:xs) = foldr
+myFoldr f z (x:xs) = f x (myFoldr f z xs)
