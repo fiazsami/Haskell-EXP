3a72e599c0297862fba6f63ee4984d3d98075050
Fiaz Sami <fiaz.sami@gmail.com>
2022-05-24 09:41:58 -0500

    deriviation of foldr

--- sessions/Folds01.hs
+++ sessions/Folds01.hs
@@ -7,3 +7,20 @@
 {-# HLINT ignore "Use foldr" #-}
 
 
+myFilter :: Foldable t => (a -> Bool) -> t a -> [a]
+myFilter fn dataSet = foldr (\x acc -> if fn x then acc else x : acc) [] dataSet 
+
+myFilter0 :: [Integer]
+myFilter0 = myFilter even [1..50]
+
+myFilter1 :: [Integer]
+myFilter1 = foldr (\x acc -> if even x then acc else x : acc) [] [1..50]
+
+fn :: Integer -> [Integer] -> [Integer]
+fn = (\x acc -> if even x then acc else x : acc)
+
+myFilter2 :: [Integer]
+myFilter2 = foldr fn [] [1..50]
+
+myFoldr f z [] = z
+myFoldr f z (x:xs) = foldr
