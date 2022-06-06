db30464d2a56f10c080e52356d94afe1a0cc49f0
Fiaz Sami <fiaz.sami@gmail.com>
2022-06-05 21:14:13 -0500

    added exhaustive functions; renamed Node -> HTML

--- sessions/Types02.hs
+++ sessions/Types02.hs
@@ -1,14 +1,12 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 type Key = String
 type Value = String
 type Name = String
 type Content = String
 
-data Attribute = Attribute Key Value | None deriving (Show, Read)
+data Attribute = Attribute Key Value deriving (Show, Read)
 data Tag = Tag Name | AttrTag Name [Attribute] deriving (Show, Read)
-data Document = Document [Document] | Node Tag [Document] | Element Tag Content deriving (Show, Read)
+data Document = Document [Document] | HTML Tag [Document] | Element Tag Content deriving (Show, Read)
 
 attr :: Attribute -> [Char]
 attr (Attribute key value) = " " ++ key ++ "='" ++ value ++ "'"
@@ -17,23 +15,24 @@ attrs :: Foldable t => t Attribute -> [Char]
 attrs = foldr(\x acc -> attr x ++ acc) []
 
 open :: Tag -> [Char]
-open (AttrTag name attributes) = "<" ++ name ++ attrs attributes ++ ">"
 open (Tag name) = "<" ++ name ++ ">"
+open (AttrTag name attributes) = "<" ++ name ++ attrs attributes ++ ">"
 
 close :: Tag -> [Char]
 close (Tag name) = "</" ++ name ++ ">"
 close (AttrTag name _) = "</" ++ name ++ ">"
 
 block :: Tag -> [Char]
+block (Tag name) = "<" ++ name ++  " />"
 block (AttrTag name attributes) = "<" ++ name ++ attrs attributes ++ " />"
 
 doc :: Document -> [Char]
 doc (Element tag content) = open tag ++ content ++ close tag
-doc (Node tag documents) = open tag ++ foldr(\x acc -> doc x ++ acc) [] documents ++ close tag
+doc (HTML tag documents) = open tag ++ foldr(\x acc -> doc x ++ acc) [] documents ++ close tag
 doc (Document documents) = foldr(\x acc -> doc x ++ acc) [] documents
 
 -- >>> doc (Document [(Element (Tag "div") "hello world"), (Element (Tag "div") "hello world")])
 -- "<div>hello world</div><div>hello world</div>"
 
--- >>> doc (Node (AttrTag "section" [Attribute "onclick" "clicked()", Attribute "style" "width:300px"]) [(Element (Tag "div") "hello world"), (Element (Tag "div") "hello world")])
--- "<section onclick='clicked()' style='width:300px'><div>hello world</div><div>hello world</div></section>"
+-- >>> doc (HTML (AttrTag "section" [Attribute "onclick" "clicked()", Attribute "style" "width:300px"]) [(Element (Tag "p") "hello world"), (Element (Tag "p") "hello world")])
+-- "<section onclick='clicked()' style='width:300px'><p>hello world</p><p>hello world</p></section>"
