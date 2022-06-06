49090cb8573367759549418b98e877dd48646291
Fiaz Sami <fiaz.sami@gmail.com>
2022-06-06 05:22:46 -0500

    removed top level siblings; partial-application/wrapping function; DTD constant

--- sessions/Types02.hs
+++ sessions/Types02.hs
@@ -1,38 +1,56 @@
-
-type Key = String
-type Value = String
 type Name = String
+type Value = String
 type Content = String
 
-data Attribute = Attribute Key Value deriving (Show, Read)
+
+dtdHTML :: [Char]
+dtdHTML = "<!DOCTYPE html>"
+
+
+data Attribute = Attribute Name Value | BoolAttr Name deriving (Show, Read)
 data Tag = Tag Name | AttrTag Name [Attribute] deriving (Show, Read)
-data Document = Document [Document] | HTML Tag [Document] | Element Tag Content deriving (Show, Read)
+data Document = HTML Tag [Document] | Element Tag Content deriving (Show, Read)
+
+
+wrap :: [a] -> [a] -> [a] -> [a]
+wrap w1 w2 item = w1 ++ item ++ w2
+wrOpen :: [Char] -> [Char]
+wrOpen = wrap "<" ">"
+wrClose :: [Char] -> [Char]
+wrClose = wrap "</" ">"
+wrBlock :: [Char] -> [Char]
+wrBlock = wrap "<" " />"
+wrName :: [Char] -> [Char]
+wrName = wrap " " "="
+wrValue :: [Char] -> [Char]
+wrValue = wrap "\"" "\""
+
+
 
 attr :: Attribute -> [Char]
-attr (Attribute key value) = " " ++ key ++ "='" ++ value ++ "'"
+attr (Attribute name value) = wrName name ++ wrValue value
+attr (BoolAttr name) = " " ++ name
 
 attrs :: Foldable t => t Attribute -> [Char]
 attrs = foldr(\x acc -> attr x ++ acc) []
 
 open :: Tag -> [Char]
-open (Tag name) = "<" ++ name ++ ">"
-open (AttrTag name attributes) = "<" ++ name ++ attrs attributes ++ ">"
+open (Tag name) = wrOpen name
+open (AttrTag name attributes) =  wrOpen name ++ attrs attributes 
 
 close :: Tag -> [Char]
-close (Tag name) = "</" ++ name ++ ">"
-close (AttrTag name _) = "</" ++ name ++ ">"
+close (Tag name) = wrClose name
+close (AttrTag name _) = wrClose name
 
 block :: Tag -> [Char]
-block (Tag name) = "<" ++ name ++  " />"
-block (AttrTag name attributes) = "<" ++ name ++ attrs attributes ++ " />"
+block (Tag name) = wrBlock name
+block (AttrTag name attributes) = wrBlock name ++ attrs attributes
 
 doc :: Document -> [Char]
 doc (Element tag content) = open tag ++ content ++ close tag
 doc (HTML tag documents) = open tag ++ foldr(\x acc -> doc x ++ acc) [] documents ++ close tag
-doc (Document documents) = foldr(\x acc -> doc x ++ acc) [] documents
 
--- >>> doc (Document [(Element (Tag "div") "hello world"), (Element (Tag "div") "hello world")])
--- "<div>hello world</div><div>hello world</div>"
+
 
 -- >>> doc (HTML (AttrTag "section" [Attribute "onclick" "clicked()", Attribute "style" "width:300px"]) [(Element (Tag "p") "hello world"), (Element (Tag "p") "hello world")])
--- "<section onclick='clicked()' style='width:300px'><p>hello world</p><p>hello world</p></section>"
+-- "<section> onclick=\"clicked()\" style=\"width:300px\"<p>hello world</p><p>hello world</p></section>"
