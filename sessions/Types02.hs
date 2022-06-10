a1cdbe6fc022c9c9cab68d058b3ebb39d4f0bd6e
Fiaz Sami <fiaz.sami@gmail.com>
2022-06-10 11:58:45 -0500

    renamed BoolElem to Singelton

--- sessions/Types02.hs
+++ sessions/Types02.hs
@@ -4,7 +4,7 @@ type Content = String
 
 data Attribute = Attr Name Value | BoolAttr Name deriving (Show, Read)
 data Tag = Tag Name | TagAttr Name [Attribute] deriving (Show, Read)
-data Document = Group [Document] | HTML Tag [Document] | Elem Tag Content | BoolElem Tag deriving (Show, Read)
+data Document = Group [Document] | HTML Tag [Document] | Elem Tag Content | Singleton Tag deriving (Show, Read)
 
 attr :: Attribute -> [Char]
 attr (Attr name value) = " " ++ name ++ "='" ++ value ++ "'"
@@ -22,7 +22,7 @@ close (Tag name) = "</" ++ name ++ ">"
 close (TagAttr name _) = "</" ++ name ++ ">"
 
 doc :: Document -> [Char]
-doc (BoolElem tag) = open tag
+doc (Singleton tag) = open tag
 doc (Elem tag content) = open tag ++ content ++ close tag
 doc (HTML tag documents) = open tag ++ doc (Group documents) ++ close tag
 doc (Group documents) = foldr(\x acc -> doc x ++ acc) [] documents
