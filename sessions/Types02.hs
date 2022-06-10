68d1dadcfa05279ad9e990c1a1fe69b13197a72b
Fiaz Sami <fiaz.sami@gmail.com>
2022-06-10 09:32:19 -0500

    updated HTML type system

--- sessions/Types02.hs
+++ sessions/Types02.hs
@@ -4,7 +4,7 @@ type Content = String
 
 data Attribute = Attr Name Value | BoolAttr Name deriving (Show, Read)
 data Tag = Tag Name | TagAttr Name [Attribute] deriving (Show, Read)
-data Document = Group [Document] | HTML Tag [Document] | Element Tag Content deriving (Show, Read)
+data Document = Group [Document] | HTML Tag [Document] | Elem Tag Content | BoolElem Tag deriving (Show, Read)
 
 attr :: Attribute -> [Char]
 attr (Attr name value) = " " ++ name ++ "='" ++ value ++ "'"
@@ -21,11 +21,9 @@ close :: Tag -> [Char]
 close (Tag name) = "</" ++ name ++ ">"
 close (TagAttr name _) = "</" ++ name ++ ">"
 
-block :: Tag -> [Char]
-block (Tag name) = "<" ++ name ++  " />"
-block (TagAttr name attributes) = "<" ++ name ++ attrs attributes ++ " />"
-
 doc :: Document -> [Char]
-doc (Element tag content) = open tag ++ content ++ close tag
+doc (BoolElem tag) = open tag
+doc (Elem tag content) = open tag ++ content ++ close tag
 doc (HTML tag documents) = open tag ++ doc (Group documents) ++ close tag
 doc (Group documents) = foldr(\x acc -> doc x ++ acc) [] documents
+
